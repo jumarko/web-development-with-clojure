c09f060ae061bfeb21490102393e6046ccf74f56
Juraj Martinka <juraj@snapshot.travel>
2017-02-15 08:32:36 +0100

    Finish section Creating Tests from book

--- examples/guestbook/test/clj/guestbook/test/db/core.clj
+++ examples/guestbook/test/clj/guestbook/test/db/core.clj
@@ -15,22 +15,20 @@
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
-(deftest test-users
+(deftest test-messages
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+    (let [timestamp (java.util.Date.)]
+      (is (= 1 (db/save-message!
+                t-conn
+                {:name "Bob"
+                 :message "Hello World"
+                 :timestamp timestamp
+                 })))
+      (is (=
+           {:name "Bob"
+            :message "Hello World"
+            :timestamp timestamp}
+           (-> (db/get-messages t-conn {})
+               first
+               (select-keys [:name :message :timestamp])))))))
