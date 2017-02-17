0daf34044f5e6b58fa8a8095cde4f8ce4463dcc7
Juraj Martinka <juraj@snapshot.travel>
2017-02-17 08:14:06 +0100

    Add "save message" feature together with some css styles.

--- examples/guestbook/src/clj/guestbook/routes/home.clj
+++ examples/guestbook/src/clj/guestbook/routes/home.clj
@@ -1,19 +1,24 @@
 (ns guestbook.routes.home
-  (:require [clojure.java.io :as io]
-            [compojure.core :refer [defroutes GET POST]]
+  (:require [compojure.core :refer [defroutes GET POST]]
             [guestbook.db.core :as db]
-            [guestbook.layout :as layout]))
+            [guestbook.layout :as layout]
+            [ring.util.http-response :as response]))
 
 (defn home-page []
   (layout/render
    "home.html"
    {:messages (db/get-messages)}))
 
+(defn save-message! [{:keys [params]}]
+  (db/save-message!
+   (assoc params :timestamp (java.util.Date.)))
+   (response/found "/"))
+
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (POST "/message" request (db/save-message! request))
+  (POST "/message" request (save-message! request))
   (GET "/about" [] (about-page)))
 
