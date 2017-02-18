ab65ae42ca24dba4c84e612d6e834480d059728a
Juraj Martinka <juraj@snapshot.travel>
2017-02-18 13:24:25 +0100

    Chapter 1 final: saving and validating new messages

--- examples/guestbook/src/clj/guestbook/routes/home.clj
+++ examples/guestbook/src/clj/guestbook/routes/home.clj
@@ -1,24 +1,42 @@
 (ns guestbook.routes.home
-  (:require [compojure.core :refer [defroutes GET POST]]
+  (:require [bouncer
+             [core :as b]
+             [validators :as v]]
+            [compojure.core :refer [defroutes GET POST]]
             [guestbook.db.core :as db]
             [guestbook.layout :as layout]
             [ring.util.http-response :as response]))
 
-(defn home-page []
+(defn home-page [{:keys [flash]}]
   (layout/render
    "home.html"
-   {:messages (db/get-messages)}))
+   (merge {:messages (db/get-messages)}
+          (select-keys flash [:name :message :errors]))))
+
+(defn validate-message
+  "Check if incoming message is valid to be stored in DB."
+  [params]
+  (first (b/validate
+   params
+   :name v/required
+   :message [v/required [v/min-count 10]])))
 
 (defn save-message! [{:keys [params]}]
-  (db/save-message!
-   (assoc params :timestamp (java.util.Date.)))
-   (response/found "/"))
+  (if-let [errors (validate-message params)]
+    (do
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    )
+    (do
+      (db/save-message!
+       (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
   (POST "/message" request (save-message! request))
   (GET "/about" [] (about-page)))
 
