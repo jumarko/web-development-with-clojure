dec907f434a2adcf1d51e3f41d1473586986d4d2
Juraj Martinka <jumarko@gmail.com>
2017-02-19 08:38:50 +0100

    Chapter 2 - ring-app: add `wrap-nocache` middleware.

--- examples/ring-app/src/ring_app/core.clj
+++ examples/ring-app/src/ring_app/core.clj
@@ -1,13 +1,22 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]))
 
 (defn handler [request-map]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str "<html><body>your IP is:" (:remote-addr request-map) "</body></html>")})
+  (prn request-map)
+  (response/response
+   (str "<html><body>your IP is:" (:remote-addr request-map) "</body></html>")))
+
+(defn wrap-nocache
+  "middleware which wraps response with 'Pragma: no-cache' header"
+  [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
 
 (defn -main []
   (jetty/run-jetty
-   handler
+   (wrap-nocache handler)
    {:port 3000
     :join? false}))
