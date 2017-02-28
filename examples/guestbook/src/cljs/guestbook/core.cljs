04a810eb70483b3eeefa5bf413178123ed17d5b9
Juraj Martinka <jumarko@gmail.com>
2017-02-28 08:21:14 +0100

    Chapter 4: Reimplement the form with Reagent

--- examples/guestbook/src/cljs/guestbook/core.cljs
+++ examples/guestbook/src/cljs/guestbook/core.cljs
@@ -1,9 +1,34 @@
 (ns guestbook.core
   (:require [reagent.core :as r :refer [atom]]))
 
-(defn home
-  []
-  [:div#hello.content>p "Hello world!"])
+;; Let's reimplement the form in Reagent
+(defn message-form []
+  (let [fields (atom {})]
+    (fn []
+      [:div.content
+       [:div.form-group
+        ;; helper element to see the actual values in atom
+        [:p "name:" (:name @fields)
+         [:p "message:" (:message @fields)]]
+        [:p "Name:"
+         [:input.form-control
+          {:type :text
+           :name :name
+           :on-change #(swap! fields assoc :name (-> % .-target .-value))
+           :value (:name @fields)}]]]
+       [:p "Message:"
+        [:textarea.form-control
+         {:rows 4
+          :cols 4
+          :name :message
+          :on-change #(swap! fields assoc :message (-> % .-target .-value))}
+         (:message @fields)]]
+       [:input.btn.btn-primary {:type :submit :value "comment"}]])))
+
+(defn home []
+  [:div row
+   [:div.span12
+    [message-form]]])
 
 (r/render
  [home]
