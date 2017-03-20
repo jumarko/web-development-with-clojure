b9e89d97344f041e2c6b279c13c1b0cb9aaf79ca
Juraj Martinka <jumarko@gmail.com>
2017-03-20 08:50:20 +0100

    Chapter 7 - Databases: add basic dependencies and showcase for simple operations.
    Simple operations like querying and inserting new records.

--- examples/db-examples/project.clj
+++ examples/db-examples/project.clj
@@ -1,6 +1,8 @@
 (defproject db-examples "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.layerware/hugsql "0.4.7"]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]])
--- examples/db-examples/src/db_examples/core.clj
+++ examples/db-examples/src/db_examples/core.clj
@@ -1,6 +1,59 @@
-(ns db-examples.core)
+(ns db-examples.core
+  (:require [clojure.java.jdbc :as sql])
+  (:import org.postgresql.ds.PGPoolingDataSource))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def db {:subprotocol "postgresql"
+         :subname "//localhost/reporting"
+         :user "admin"
+         :password "admin"})
+
+;; another option is to provide a JDBC data source and configure it manually
+;; -> useful for any driver-specific parameters not accessible through the idiomatic
+;;    parameter map
+#_(def db
+    {:datasource
+     (doto (PGPoolingDataSource.)
+       (.setServerName "localhost")
+       (.setDatabaseName "my_website")
+       (.setUser "admin")
+       (.setPassword "admin")
+       (.setMaxConnections 10))})
+
+;; finally, we can define a JNDI string for a connection managed by application server
+#_(def db {:name "jdbc/myDatasource"})
+
+;;; Creating tables
+
+(defn create-users-table! []
+  (sql/db-do-commands
+   db
+   (sql/create-table-ddl
+    :users
+    [[:id "varchar(32) PRIMARY KEY"]
+     [:pass "varchar(100)"]])))
+
+(try (create-users-table!)
+     (catch Exception e (.getNextException e)))
+
+;;; Selecting Records
+;;; query function allows us to work with returned data without having to load the entire result into memory
+;;; (lazy sequence)
+
+(defn get-user [id]
+  (first (sql/query db ["select * from users where id = ?" id])))
+(get-user "foo")
+
+;;; Inserting Records
+
+;; insert!
+(defn add-user! [user]
+  (sql/insert! db :users user))
+(add-user! {:id "foo" :pass "bar"})
+
+;; insert multiple records - insert-multi!
+(defn add-users! [& users]
+  (sql/insert-multi! db :users users))
+(add-users!
+ {:id "foo1" :pass "bar"}
+ {:id "foo2" :pass "bar"}
+ {:id "foo3" :pass "bar"})
